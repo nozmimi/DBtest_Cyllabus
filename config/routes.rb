35d3456b068ee8b16eaefb003c0589e810c6eed3
nozmimi <nozmimi@yahoo.co.jp>
2018-10-09 15:49:29 +0000

    細かい装飾の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
     
-  get 'boards' => 'boards#index'
-  get 'boards/new' => 'boards#new'
-  post 'boards' => 'boards#create'
-  get 'boards/:id' => 'boards#show'
-  get 'boards/:id/edit' => 'boards#edit'
-  put 'boards/:id' => 'boards#update'
-  delete 'boards/:id' => 'boards#destroy'
+  # get 'boards' => 'boards#index'
+  # get 'boards/new' => 'boards#new'
+  # post 'boards' => 'boards#create'
+  # get 'boards/:id' => 'boards#show'
+  # get 'boards/:id/edit' => 'boards#edit'
+  # put 'boards/:id' => 'boards#update'
+  # delete 'boards/:id' => 'boards#destroy'
+
+  resources :boards
   
 end
